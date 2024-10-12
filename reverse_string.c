ed7a0ce2754474591b7d481997502a0f8ef82388
Sahili15 <166319076+Sahili15@users.noreply.github.com>
2024-10-13 02:26:26 +0530

    Add files via upload

---
+++ reverse_string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char str[]="hello";
+    char reversed[100];
+    int length,i;
+    for(length=0;str[length]!='\0';length++);
+    for(i=0;i<length;i++){
+        reversed[i]=str[length-1-i];
+    }
+    reversed[length]='\0';
+    printf("reversed string:%s\n",reversed);
+    return 0;
+}
